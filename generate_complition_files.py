6e563fc47f17c19c327f9a247ad5b16ade0ee546
Manuel MAGALHAES <valkaari@Mac-Pro-de-Manuel.local>
2016-10-12 17:37:09 +0200

    update script and package
    update package for r18 and update the generator.

--- generate_complition_files.py
+++ generate_complition_files.py
@@ -1,6 +1,8 @@
-import sys, os
+import sys, os, getopt
+import argparse
 import shutil
 import re
+import time
 
 class GetPythonObject():
     
@@ -9,13 +11,14 @@ class GetPythonObject():
     nbdir = 0
     nbfile = 0
     nbline = 0
-    def __init__(self):
-        self.pythonSDKDir = "/Volumes/Stock_01/Users/valkaari/Documents/Developpement/HTML SDK/CINEMA4DR16021PYTHONSDKHTML20141003/help/modules"
-        self.exportDir = "/Volumes/Stock_01/Users/valkaari/Desktop/c4dPython"
-        
+    def __init__(self,inputDirectory, outputDirectory):
+        self.pythonSDKDir = inputDirectory
+        self.exportDir = outputDirectory
+       
    
-    def exportModule(self, folder):
-    
+    def exportModule(self, folder = ""):
+        if folder == "":
+            folder = self.pythonSDKDir
         dirList=os.listdir(folder)
       
         for fname in dirList:
@@ -34,13 +37,13 @@ class GetPythonObject():
                 fn.write("{\n\t\t\"scope\" : \"source.pythonAPI.cinema4D\",\n\t\t\"completions\" : [\n ")
 
                 #load file in a string
-                pattern = re.compile(r'<tt class="descclassname">(.+)<a')
+                pattern = re.compile(r'<code class="descclassname">(.+)<a')
                 cleanhtml = re.compile('<.*?>')
                 for i, line in enumerate(open(os.path.join(folder , fname), "r")):
                     match = re.findall(pattern, line)
                     if match:
                         cleanFunction = re.sub(cleanhtml, '',match[0])
-                        cleanFunction = cleanFunction.replace("&quot;" , "\"")
+                        cleanFunction = cleanFunction.replace("&quot;" , "\\\"")
                         patParam = re.compile(r'\(.+\)')
                         parameters = re.search(patParam, cleanFunction)
 
@@ -55,7 +58,7 @@ class GetPythonObject():
                         self.nbline+=1
 
                 #before closing, add a dummy elmt
-                fn.write("  \t\t\t\t{ \"trigger\": \"____zzzzzdummy\", \"contents\": \"_____zzzzzdummy\" } ")
+                fn.write("  \t\t\t\t{ \"trigger\": \"____zdummy\", \"contents\": \"_____zdummy\" } ")
                 self.nbline+=1
                 fn.write ("\n\t\t]\n}")
                 self.nbline+=1        
@@ -68,7 +71,7 @@ class GetPythonObject():
         res  = re.sub(r'(\[.+?\])', r' {}:\1' , res)
         res  = res.format(*list(i for i in range(1,50)))
         res  = re.sub(r'(\d:\w+|\d:\[.+?\])' , r'${\1}', res)   
-        res  = re.sub('"', r'\"', res)
+        #res  = re.sub('"', r'\"', res) 
         return res
 
 
@@ -80,38 +83,29 @@ class GetPythonObject():
             else:
                 os.remove(os.path.join(self.exportDir, fname))
 
-    def test(self, folder = None):
-        if not folder:
-            folder = self.pythonSDKDir
 
-        dirList=os.listdir(folder)
-        for fname in dirList:
-           if os.path.isdir(os.path.join(folder , fname)):
-                #if this is a directory, create the directory
-                print os.path.join(folder , fname)[len(self.pythonSDKDir):]
-                #and go sub folder
-                self.test(os.path.join(folder , fname))
 
 
     def getStat(self):
-        print "nombre de fichiers : " ,self.nbfile
-        print "nombre de repertoires : ", self.nbdir
-        print "nombre de lignes : ", self.nbline
+        print "files : " ,self.nbfile
+        print "directories : ", self.nbdir
+        print "lines  : ", self.nbline
 
-def main():
-    print "let's go"
-    getObjects = GetPythonObject()
-    #getObjects.test()
+def main(args):
+    start_time = time.time()
+    getObjects = GetPythonObject(args.inputDirectory, args.outputDirectory)
     getObjects.deleteDirectory()
-    getObjects.exportModule("/Volumes/Stock_01/Users/valkaari/Documents/Developpement/HTML SDK/CINEMA4DR16021PYTHONSDKHTML20141003/help/modules")
+    getObjects.exportModule()
     getObjects.getStat()    
 
-    
-    print "done"
-    
+    print ("done in %s seconds" % (time.time() - start_time))
     
 
 
     
 if __name__=='__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Create Sublime Package from Cinema4D\'s documentation' )
+    parser.add_argument("inputDirectory",help = "the cinema4D's python SDK help directory doc->html->modules")
+    parser.add_argument("outputDirectory", help = "The directory to store the result")
+    args = parser.parse_args()
+    main(args)
